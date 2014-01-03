d9cc0d8fc1d573d08e45fb2395dba1a00e6508de
salone <salonegupta@gmail.com>
2014-01-03 14:58:09 +0530

    BPMS-1081: Added a snippet to remove intalio_build_env file if it exists.

--- intalio-common-function.rb
+++ intalio-common-function.rb
@@ -1,11 +1,18 @@
 #Utility Class which is used for common functions required during build process
 
+HUDSON_PROPERTIES = "hudsonBuild.properties"
+INTALIO_BUILD_ENV = "INTALIO_BUILD_ENV"
+
 class CommonFunctions
 	def self.getIntalioVersionFile()
-		if(File.exists?("hudsonBuild.properties"))
-			cp "hudsonBuild.properties", "INTALIO_BUILD_ENV"
+		if(File.exists?(INTALIO_BUILD_ENV))
+			rm INTALIO_BUILD_ENV
 		end
 
-		return "INTALIO_VERSION"
+		if(File.exists?(HUDSON_PROPERTIES))
+			cp HUDSON_PROPERTIES, INTALIO_BUILD_ENV
+		end
+
+		return INTALIO_BUILD_ENV
 	end
 end
