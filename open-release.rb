7a485c804d3670974a9af5c4f024dc8f9a294a16
Veresh Jain <veresh@intalio.com>
2014-03-26 14:16:10 +0530

    Updated the username for uploading the artifacts

--- open-release.rb
+++ open-release.rb
@@ -1,3 +1,3 @@
-repositories.release_to[:username] ||= "release"
+repositories.release_to[:username] ||= "ubuntu"
 repositories.release_to[:url] ||= "sftp://www.intalio.org/var/www-org/public/maven2"
 repositories.release_to[:permissions] ||= 0664
