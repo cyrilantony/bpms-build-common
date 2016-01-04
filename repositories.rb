1539389616d802b6658a901bebe3e983ef2ec67d
Himanshu Mishra <mishra@intalio.com>
2016-01-04 14:40:32 +0530

    Updating repository URL

--- repositories.rb
+++ repositories.rb
@@ -1,5 +1,5 @@
-repositories.remote << "http://bpms-build-blr.local:8081/nexus/content/repositories/m2repo"
+repositories.remote << "http://bpms-build-blr:8081/nexus/content/repositories/m2repo"
 
 repositories.release_to[:username] ||= "intalio"
 repositories.release_to[:password] ||= "intalio321"
-repositories.release_to[:url] ||= "sftp://intalio@bpms-build-blr.local/media/disk1/repository"
+repositories.release_to[:url] ||= "sftp://intalio@bpms-build-blr/media/disk1/repository"
