ddb470126da9555bf55f7ba2e14123778713adb1
Cyril Antony <c.antony@everteam>
2017-02-28 16:23:39 +0530

    BPMS-1913: Add commons-collections-4 jar

--- common.rb
+++ common.rb
@@ -18,6 +18,9 @@ APACHE_COMMONS = {
   :primitives       =>"commons-primitives:commons-primitives:jar:1.0",
   :validator => "commons-validator:commons-validator:jar:1.2.0"
 }
+APACHE_COMMONS_4 = {
+  :collections => "org.apache.commons:commons-collections4:jar:4.1"
+}
 APACHE_DERBY = "org.apache.derby:derby:jar:#{DERBY_VER}"
 APACHE_DERBY_NET = "org.apache.derby:derbynet:jar:#{DERBY_VER}"
 APACHE_DERBY_CLIENT = "org.apache.derby:derbyclient:jar:#{DERBY_VER}"
